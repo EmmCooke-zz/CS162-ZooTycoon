9b1ea01fa5f54ea7d8f0d5131c3a5c798612225d
Emmet <emmetcooke@gmail.com>
2017-10-20 15:17:37 -0500

    Filled in the Turtle class

--- Project2/Turtle.cpp
+++ Project2/Turtle.cpp
@@ -1,12 +1,50 @@
+/*************************************************
+* Author: Emmet Cooke
+* Date: 10/20/2017
+* Description: This file contains the definitions for
+* the Turtle class.
+*************************************************/
 #include "Turtle.hpp"
 
+/*************************************************
+* Description: Default Constructor. For babies.
+*************************************************/
+Turtle::Turtle() : Animal(1)
+{
+	Turtle::setNumBabies(10);
+	Turtle::setCost(100.00);
+	Turtle::setBaseFoodCost(0.5);
+	Turtle::setPayoff(0.05);
+}
 
+/*************************************************
+* Description: Constructor that takes in one int for
+* age and passes it to the Animal constructor. For
+* adults.
+*************************************************/
+Turtle::Turtle(int ageIn)
+	: Animal(ageIn)
+{
+	Turtle::setNumBabies(10);
+	Turtle::setCost(100.00);
+	Turtle::setBaseFoodCost(0.5);
+	Turtle::setPayoff(0.05);
+}
 
-Turtle::Turtle()
+/*************************************************
+* Description: Destructor.
+*************************************************/
+Turtle::~Turtle()
 {
 }
 
-
-Turtle::~Turtle()
+/*************************************************
+* Description: Overloading the postfix ++ operator
+* so that it is easier to increase the Turtles age.
+*************************************************/
+Turtle Turtle::operator+(int)
 {
+	Turtle temp = *this;
+	age++;
+	return temp;
 }
--- Project2/Turtle.hpp
+++ Project2/Turtle.hpp
@@ -1,10 +1,41 @@
-#pragma once
+/*************************************************
+* Author: Emmet Cooke
+* Date: 10/20/2017
+* Description: This is the header file for the Turtle
+* class.
+*************************************************/
+#ifndef Turtle_HPP
+#define Turtle_HPP
+
 #include "Animal.hpp"
 class Turtle :
 	public Animal
 {
 public:
+	// Default Constructor
 	Turtle();
+
+	// Constructor that takes one int
+	// Initializes an bought adult tiger
+	Turtle(int ageIn);
+
+	// Destructor
 	~Turtle();
+
+	// Getters
+	virtual int getNumBabies() { return numBabies; }
+	virtual double getCost() { return cost; }
+	virtual double getBaseFoodCost() { return baseFoodCost; }
+	virtual double getPayoff() { return payoff; }
+
+	// Setters
+	virtual void setNumBabies(int babiesIn) { numBabies = babiesIn; }
+	virtual void setCost(double costIn) { cost = costIn; }
+	virtual void setBaseFoodCost(double foodCostIn) { baseFoodCost = foodCostIn; }
+	virtual void setPayoff(double payoffIn) { payoff = payoffIn; }
+
+	// Increase age
+	Turtle operator+(int);
 };
 
+#endif

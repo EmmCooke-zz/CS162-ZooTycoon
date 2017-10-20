420ca0ed37e02a4c43d6e3f5fbc29a51fbc10cdb
Emmet <emmetcooke@gmail.com>
2017-10-20 15:13:38 -0500

    Filled in the Penguin class

--- Project2/Penguin.cpp
+++ Project2/Penguin.cpp
@@ -1,12 +1,50 @@
+/*************************************************
+* Author: Emmet Cooke
+* Date: 10/20/2017
+* Description: This file contains the definitions for
+* the Penguin class.
+*************************************************/
 #include "Penguin.hpp"
 
+/*************************************************
+* Description: Default Constructor. For babies.
+*************************************************/
+Penguin::Penguin() : Animal(1)
+{
+	Penguin::setNumBabies(5);
+	Penguin::setCost(1000.00);
+	Penguin::setBaseFoodCost(1);
+	Penguin::setPayoff(0.1);
+}
 
+/*************************************************
+* Description: Constructor that takes in one int for
+* age and passes it to the Animal constructor. For
+* adults.
+*************************************************/
+Penguin::Penguin(int ageIn)
+	: Animal(ageIn)
+{
+	Penguin::setNumBabies(5);
+	Penguin::setCost(1000.00);
+	Penguin::setBaseFoodCost(1);
+	Penguin::setPayoff(0.1);
+}
 
-Penguin::Penguin()
+/*************************************************
+* Description: Destructor.
+*************************************************/
+Penguin::~Penguin()
 {
 }
 
-
-Penguin::~Penguin()
+/*************************************************
+* Description: Overloading the postfix ++ operator
+* so that it is easier to increase the Penguins age.
+*************************************************/
+Penguin Penguin::operator+(int)
 {
+	Penguin temp = *this;
+	age++;
+	return temp;
 }
--- Project2/Penguin.hpp
+++ Project2/Penguin.hpp
@@ -1,10 +1,41 @@
-#pragma once
+/*************************************************
+* Author: Emmet Cooke
+* Date: 10/20/2017
+* Description: This is the header file for the Penguin
+* class.
+*************************************************/
+#ifndef PENGUIN_HPP
+#define PENGUIN_HPP
+
 #include "Animal.hpp"
 class Penguin :
 	public Animal
 {
 public:
+	// Default Constructor
 	Penguin();
+
+	// Constructor that takes one int
+	// Initializes an bought adult tiger
+	Penguin(int ageIn);
+
+	// Destructor
 	~Penguin();
+
+	// Getters
+	virtual int getNumBabies() { return numBabies; }
+	virtual double getCost() { return cost; }
+	virtual double getBaseFoodCost() { return baseFoodCost; }
+	virtual double getPayoff() { return payoff; }
+
+	// Setters
+	virtual void setNumBabies(int babiesIn) { numBabies = babiesIn; }
+	virtual void setCost(double costIn) { cost = costIn; }
+	virtual void setBaseFoodCost(double foodCostIn) { baseFoodCost = foodCostIn; }
+	virtual void setPayoff(double payoffIn) { payoff = payoffIn; }
+
+	// Increase age
+	Penguin operator+(int);
 };
 
+#endif
